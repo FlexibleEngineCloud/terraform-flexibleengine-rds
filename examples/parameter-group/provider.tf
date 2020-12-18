07bd709f785b99a78f5294334a2dbdb206492d86
olivier <olivier.saluden@orange.com>
2020-12-18 13:37:58 +0100

    Enhance documenation for paramter group

---
+++ examples/parameter-group/provider.tf
@@ -0,0 +1,8 @@
+provider "flexibleengine" {
+  domain_name = "XXXXXX"
+  access_key  = "XXX"
+  secret_key  = "XXX"
+  tenant_name = "eu-west-0"
+  auth_url    = "https://iam.eu-west-0.prod-cloud-ocb.orange-business.com/v3"
+  region      = "eu-west-0"
+}
